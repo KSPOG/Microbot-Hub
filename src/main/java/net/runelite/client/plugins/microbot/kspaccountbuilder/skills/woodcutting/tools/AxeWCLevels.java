a68e3e4b58dea23ca881135c2e4df159cdb9316d
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-14 19:50:03 +0100

    feat: add GE buy helper for missing woodcutting axes

---
+++ src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/skills/woodcutting/tools/AxeWCLevels.java
@@ -0,0 +1,18 @@
+package net.runelite.client.plugins.microbot.kspaccountbuilder.skills.woodcutting.tools;
+
+/**
+ * Woodcutting level requirements for axes used by KSPAccountBuilder.
+ */
+public final class AxeWCLevels {
+
+    private AxeWCLevels() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static final int BRONZE_AXE = 1;
+    public static final int STEEL_AXE = 6;
+    public static final int BLACK_AXE = 11;
+    public static final int MITHRIL_AXE = 21;
+    public static final int ADAMANT_AXE = 31;
+    public static final int RUNE_AXE = 41;
+}
