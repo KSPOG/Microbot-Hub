9dcfceb3b7f3b22b33e845364c2faff3dae922e9
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-14 04:38:51 +0100

    updated to latest version
    had to download the latest files

--- src/test/java/net/runelite/client/Microbot.java
+++ src/test/java/net/runelite/client/Microbot.java
@@ -1,18 +1,16 @@
 package net.runelite.client;
 
-
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import net.runelite.client.plugins.microbot.customwebwalker.CustomWebWalkerPlugin;
+import net.runelite.client.plugins.microbot.slayer.SlayerPlugin;
 
 public class Microbot
 {
 
 	private static final Class<?>[] debugPlugins = {
-            CustomWebWalkerPlugin.class
+            SlayerPlugin.class,
 	};
 
     public static void main(String[] args) throws Exception
