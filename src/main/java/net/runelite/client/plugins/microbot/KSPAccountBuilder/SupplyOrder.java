d6e1a44f5f3d9c93e3304ec7e0217f994e1cc34f
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-14 05:55:07 +0100

    Delete src/main/java/net/runelite/client/plugins/microbot/KSPAccountBuilder directory

--- src/main/java/net/runelite/client/plugins/microbot/KSPAccountBuilder/SupplyOrder.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.KSPAccountBuilder;
-
-final class SupplyOrder {
-    final String itemName;
-
-    SupplyOrder(String itemName) {
-        this.itemName = itemName;
-    }
-}
