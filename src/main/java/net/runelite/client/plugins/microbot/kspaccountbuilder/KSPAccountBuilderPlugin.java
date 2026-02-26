184a8ed7e50465ece388d850fc52cb20100c7802
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-26 14:49:30 +0100

    fix: skip startup banking when task supplies are already ready

--- src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/KSPAccountBuilderPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/KSPAccountBuilderPlugin.java
@@ -24,7 +24,7 @@ import java.awt.AWTException;
 @Slf4j
 public class KSPAccountBuilderPlugin extends Plugin {
 
-    public static final String VERSION = "0.0.47";
+    public static final String VERSION = "0.0.52";
 
 
 
