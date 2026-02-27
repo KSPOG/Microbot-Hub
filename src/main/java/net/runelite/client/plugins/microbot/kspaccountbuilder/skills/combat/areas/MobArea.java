ae11e206c2f352ff99e2006b56b15b93548f2155
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-27 20:25:52 +0100

    again removing files
    Once again removing files i don't need

---
+++ src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/skills/combat/areas/MobArea.java
@@ -0,0 +1,29 @@
+package net.runelite.client.plugins.microbot.kspaccountbuilder.skills.combat.areas;
+
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.coords.WorldPoint;
+
+public final class MobArea {
+    private MobArea() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Goblins area bounds: (3239,3217) to (3266,3243), plane 0.
+     */
+    public static final WorldArea GOBLINS =
+            new WorldArea(new WorldPoint(3239, 3217, 0), 28, 27);
+
+    /**
+     * Cows area bounds: (3253,3255) to (3265,3296), plane 0.
+     */
+    public static final WorldArea COWS =
+            new WorldArea(new WorldPoint(3253, 3255, 0), 13, 42);
+
+    /**
+     * Al-Kharid Guards area bounds: (3287,3167) to (3298,3177), plane 0.
+     */
+    public static final WorldArea AL_KHARID_GUARDS =
+            new WorldArea(new WorldPoint(3287, 3167, 0), 12, 11);
+
+}
---
+++ src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/skills/combat/needed/Gear.java
@@ -0,0 +1,64 @@
+package net.runelite.client.plugins.microbot.kspaccountbuilder.skills.combat.needed;
+
+import net.runelite.api.ItemID;
+import net.runelite.client.plugins.microbot.kspaccountbuilder.skills.combat.gear.armour.Armour;
+import net.runelite.client.plugins.microbot.kspaccountbuilder.skills.combat.gear.swords.Swords;
+
+public final class Gear {
+    private Gear() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static final int MIN_TROUT_REQUIRED = 5;
+
+    public static final int AMULET_OF_POWER = ItemID.AMULET_OF_POWER;
+
+    public static final int[] FOOD_REQUIREMENTS = {
+            ItemID.TROUT
+    };
+
+    /**
+     * Any team cape is accepted, these are the standard wilderness team capes.
+     */
+    public static final String[] TEAM_CAPE_NAME_MATCHERS = {
+            "Team-",
+            "Team cape"
+    };
+
+    public static final int[] BEST_MELEE_WEAPONS_BY_LEVEL = {
+            Swords.RUNE_SCIMITAR,
+            Swords.ADAMANT_SCIMITAR,
+            Swords.MITHRIL_SCIMITAR,
+            Swords.BLACK_SCIMITAR,
+            Swords.STEEL_SCIMITAR,
+            Swords.IRON_SCIMITAR,
+            Swords.BRONZE_SCIMITAR
+    };
+
+    public static final int[] BEST_MELEE_ARMOUR_BY_LEVEL = {
+            Armour.RUNE_FULL_HELM,
+            Armour.RUNE_PLATEBODY,
+            Armour.RUNE_PLATELEGS,
+            Armour.RUNE_KITESHIELD,
+            Armour.ADAMANT_FULL_HELM,
+            Armour.ADAMANT_PLATEBODY,
+            Armour.ADAMANT_PLATELEGS,
+            Armour.ADAMANT_KITESHIELD,
+            Armour.MITHRIL_FULL_HELM,
+            Armour.MITHRIL_PLATEBODY,
+            Armour.MITHRIL_PLATELEGS,
+            Armour.MITHRIL_KITESHIELD,
+            Armour.BLACK_FULL_HELM,
+            Armour.BLACK_PLATEBODY,
+            Armour.BLACK_PLATELEGS,
+            Armour.BLACK_KITESHIELD,
+            Armour.IRON_FULL_HELM,
+            Armour.IRON_PLATEBODY,
+            Armour.IRON_PLATELEGS,
+            Armour.IRON_KITESHIELD,
+            Armour.BRONZE_FULL_HELM,
+            Armour.BRONZE_PLATEBODY,
+            Armour.BRONZE_PLATELEGS,
+            Armour.BRONZE_KITESHIELD
+    };
+}
