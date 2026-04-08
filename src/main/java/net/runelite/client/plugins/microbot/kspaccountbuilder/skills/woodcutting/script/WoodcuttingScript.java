2d12e0cd279c9e25bab7d5e35ad7536ee6150f93
KSPOG <kratosmaximusosrs@gmail.com>
2026-04-08 21:42:52 +0200

    uneeded files
    removing files that aren't needed

--- src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/skills/woodcutting/script/WoodcuttingScript.java
+++
@@ -1,4 +0,0 @@
-package net.runelite.client.plugins.microbot.kspaccountbuilder.skills.woodcutting.script;
-
-public class WoodcuttingScript {
-}
--- src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/skills/woodcutting/states/State.java
+++
@@ -1,4 +0,0 @@
-package net.runelite.client.plugins.microbot.kspaccountbuilder.skills.woodcutting.states;
-
-public class State {
-}
