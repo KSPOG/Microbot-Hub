27f7c2d77213f98de74f892cb9a74d000750aff5
KSPOG <kratosmaximusosrs@gmail.com>
2026-04-15 18:24:10 +0200

    Delete src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder directory

--- src/main/java/net/runelite/client/plugins/microbot/kspaccountbuilder/tasks/skilling/selling/sellscript/SellState.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.microbot.kspaccountbuilder.tasks.skilling.selling.sellscript;
-
-enum SellState
-{
-    GOING_TO_GE,
-    RESTOCKING_FROM_BANK,
-    MONITORING_COPILOT
-}
