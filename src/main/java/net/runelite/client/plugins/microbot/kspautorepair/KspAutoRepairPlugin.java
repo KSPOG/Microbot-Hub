6c879ac09556255a861f4acaee7f6f47083fd82d
KSPOG <kratosmaximusosrs@gmail.com>
2026-08-15 01:30:22 +0200

    feat: wire v0.2 event-driven reliability coordinator

--- src/main/java/net/runelite/client/plugins/microbot/kspautorepair/KspAutoRepairPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/kspautorepair/KspAutoRepairPlugin.java
@@ -2,7 +2,20 @@ package net.runelite.client.plugins.microbot.kspautorepair;
 
 import com.google.inject.Provides;
 import net.runelite.api.Client;
+import net.runelite.api.events.AnimationChanged;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameObjectDespawned;
+import net.runelite.api.events.GameObjectSpawned;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.api.events.NpcDespawned;
+import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.VarbitChanged;
+import net.runelite.api.events.WidgetClosed;
+import net.runelite.api.events.WidgetLoaded;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -14,8 +27,8 @@ import javax.inject.Inject;
 
 @PluginDescriptor(
         name = PluginConstants.KSP + "Auto Repair Agent",
-        description = "Autonomously observes KSP plugins, diagnoses failures and sends validated source fixes back through KSP Source Loader",
-        tags = {"ksp", "agent", "repair", "debug", "autonomous", "development"},
+        description = "Autonomously observes KSP intent, actions and game-state results and validates self-healing source repairs",
+        tags = {"ksp", "agent", "repair", "debug", "autonomous", "development", "observer"},
         version = KspAutoRepairPlugin.VERSION,
         cardUrl = "",
         iconUrl = "",
@@ -25,7 +38,7 @@ import javax.inject.Inject;
 )
 public class KspAutoRepairPlugin extends Plugin
 {
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 
     @Inject
     private Client client;
@@ -36,7 +49,7 @@ public class KspAutoRepairPlugin extends Plugin
     @Inject
     private KspAutoRepairConfig config;
 
-    private KspAutoRepairService service;
+    private KspAutoRepairCoordinator coordinator;
 
     @Provides
     KspAutoRepairConfig provideConfig(ConfigManager configManager)
@@ -47,27 +60,115 @@ public class KspAutoRepairPlugin extends Plugin
     @Override
     protected void startUp()
     {
-        service = new KspAutoRepairService(client, pluginManager, config);
-        service.start();
+        coordinator = new KspAutoRepairCoordinator(client, pluginManager, config);
+        coordinator.start();
     }
 
     @Override
     protected void shutDown()
     {
-        if (service != null)
+        if (coordinator != null)
         {
-            service.stop();
-            service = null;
+            coordinator.stop();
+            coordinator = null;
         }
     }
 
     @Subscribe
     public void onGameTick(GameTick event)
     {
-        KspAutoRepairService current = service;
-        if (current != null)
-        {
-            current.onGameTick();
-        }
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onGameTick();
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onGameStateChanged(event);
+    }
+
+    @Subscribe
+    public void onItemContainerChanged(ItemContainerChanged event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onItemContainerChanged(event);
+    }
+
+    @Subscribe
+    public void onVarbitChanged(VarbitChanged event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onVarbitChanged(event);
+    }
+
+    @Subscribe
+    public void onWidgetLoaded(WidgetLoaded event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onWidgetLoaded(event);
+    }
+
+    @Subscribe
+    public void onWidgetClosed(WidgetClosed event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onWidgetClosed(event);
+    }
+
+    @Subscribe
+    public void onMenuOptionClicked(MenuOptionClicked event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onMenuOptionClicked(event);
+    }
+
+    @Subscribe
+    public void onNpcSpawned(NpcSpawned event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onNpcSpawned(event);
+    }
+
+    @Subscribe
+    public void onNpcDespawned(NpcDespawned event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onNpcDespawned(event);
+    }
+
+    @Subscribe
+    public void onGameObjectSpawned(GameObjectSpawned event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onGameObjectSpawned(event);
+    }
+
+    @Subscribe
+    public void onGameObjectDespawned(GameObjectDespawned event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onGameObjectDespawned(event);
+    }
+
+    @Subscribe
+    public void onAnimationChanged(AnimationChanged event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onAnimationChanged(event);
+    }
+
+    @Subscribe
+    public void onChatMessage(ChatMessage event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onChatMessage(event);
+    }
+
+    @Subscribe
+    public void onStatChanged(StatChanged event)
+    {
+        KspAutoRepairCoordinator current = coordinator;
+        if (current != null) current.onStatChanged(event);
     }
 }
